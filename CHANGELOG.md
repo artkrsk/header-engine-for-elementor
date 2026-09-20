2ebb7231e256b8c8d4543c2a3a706c9b4856e69c
Artem Semkin <art.krsk@gmail.com>
2026-09-20 16:12:05 +0800

    Release 1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## 1.0.1
+
+* improved: sticky and auto-hide header performance during page load and scrolling.
+* improved: header reliability on AJAX-driven page transitions.
+
 ## 1.0.0
 
 Initial release.
--- src/wordpress-plugin/artem-semkin-header-engine-for-elementor.php
+++ src/wordpress-plugin/artem-semkin-header-engine-for-elementor.php
@@ -3,7 +3,7 @@
  * Plugin Name: Artem Semkin Header Engine for Elementor
  * Plugin URI: https://artemsemkin.com/plugins/header-engine-for-elementor/
  * Description: Sticky and auto-hide header for Elementor containers. Hide on scroll down, reveal on scroll up, logo swap, sticky styling. Free, zero dependencies.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Artem Semkin
  * Author URI: https://artemsemkin.com
  * Requires at least: 6.5
@@ -19,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'ARTS_HEADER_PLUGIN_VERSION', '1.0.0' );
+define( 'ARTS_HEADER_PLUGIN_VERSION', '1.0.1' );
 define( 'ARTS_HEADER_PLUGIN_FILE', __FILE__ );
 
 require_once __DIR__ . '/vendor/autoload.php';
